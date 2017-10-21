08a297fa6d941774876c3754cec20ddf75d2db37
LynxEyes <ivo.jesus@gmail.com>
2017-10-22 00:32:23 +0100

    Initial commit - lein new dotenv

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dotenv
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dotenv/core_test.clj
@@ -0,0 +1,7 @@
+(ns dotenv.core-test
+  (:require [clojure.test :refer :all]
+            [dotenv.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
